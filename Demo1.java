aa2c6645bc2dd36fbb7c6d208f2bbc06d0d145dd
Neha131 <70276108+Neha131@users.noreply.github.com>
2020-09-01 23:09:58 +0530

    Update Demo1.java
    Second Version

--- Demo1.java
+++ Demo1.java
@@ -4,5 +4,7 @@ public class Demo1
 	{
 		System.out.println("My First Java Code....");
 		System.out.println("GeeksForGeeks is best to learn java...");
+		System.out.println("This is my Second version....");
+		System.out.println("Also learn HTML and CSS for web desinging...");
 	}
 }
